3619a0bd9e92186542df6d18d397d982e80785a1
salomaosilval <salomaosilvadev@gmail.com>
2025-02-21 19:36:21 -0400

    feat: add container component for responsive layout

---
+++ app/_components/ui/container.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/app/_lib/utils";
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export function Container({ children, className, ...props }: ContainerProps) {
+  return (
+    <div
+      className={cn("w-full max-w-[1200px] mx-auto px-4 md:px-6", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
